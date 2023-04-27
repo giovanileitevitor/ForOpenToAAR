169e895b7922b9d5ddf57dbb20f31e4b7f526277
Giovani Leite Vitor <giovani.vitor@timwe.com>
2023-04-27 09:49:30 -0300

    ajuste no gradle

---
+++ app/src/main/java/com/babel/foropentoaar/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.babel.foropentoaar
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.babel.foropentoaar.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import com.timwe.init.Tti2
+import com.timwe.tti2sdk.data.model.response.UserProfile
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+            val tti2 = Tti2()
+            val userProfile =  UserProfile()
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
